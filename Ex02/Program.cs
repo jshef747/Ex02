a535b0337d0bc0d5121abbe4f20e129bf72a5ba2
Jonathan Sheffer <j.sheffer747@gmail.com>
2025-05-11 13:05:10 +0300

    added RandomGameWord class to generate unique random words

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -4,13 +4,20 @@ class Program
 {
     public static void Main()
     {
-        string check = Console.ReadLine();
-        bool isValid = InputValidator.IsValidGuessInput(check);
-        string? why = InputValidator.BadInputMessage;
-        Console.WriteLine($"your input: {isValid}\nbad input reason: {why}");
-        check = Console.ReadLine();
-        isValid = InputValidator.IsValidGuessNumber(check);
-        why = InputValidator.BadInputMessage;
-        Console.WriteLine($"your input: {isValid}\nbad input reason: {why}");
+        // string check = Console.ReadLine();
+        // bool isValid = InputValidator.IsValidGuessInput(check);
+        // string? why = InputValidator.BadInputMessage;
+        // Console.WriteLine($"your input: {isValid}\nbad input reason: {why}");
+        // check = Console.ReadLine();
+        // isValid = InputValidator.IsValidGuessNumber(check);
+        // why = InputValidator.BadInputMessage;
+        // Console.WriteLine($"your input: {isValid}\nbad input reason: {why}");
+        
+        RandomGameWord randomGameWord = new RandomGameWord();
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(randomGameWord.RandomWord);
+            randomGameWord.GenerateRandomWord();
+        }
     }
 }
---
+++ Ex02/RandomGameWord.cs
@@ -0,0 +1,43 @@
+namespace Ex02;
+
+public class RandomGameWord
+{
+    private enum eValidCharaters
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H
+    }
+
+    private const int k_ValidCharactersLength = 8;
+    private const int k_RandomWordLength = 4;
+    
+    public string? RandomWord { private set; get; }
+
+    public RandomGameWord()
+    {
+        GenerateRandomWord();
+    }
+    
+    public void GenerateRandomWord()
+    {
+        Random random = new Random();
+        List<char> charsForRandomWord = new List<char>();
+        while(charsForRandomWord.Count < k_RandomWordLength)
+        {
+            int randomValue = random.Next(0, k_ValidCharactersLength);
+            eValidCharaters character = (eValidCharaters)randomValue;
+            char charToBeCheckedAndAdded = character.ToString()[0];
+            if (!charsForRandomWord.Contains(charToBeCheckedAndAdded))
+            {
+                charsForRandomWord.Add(charToBeCheckedAndAdded);
+            }
+        }
+        RandomWord = new string(charsForRandomWord.ToArray());
+    }
+}
